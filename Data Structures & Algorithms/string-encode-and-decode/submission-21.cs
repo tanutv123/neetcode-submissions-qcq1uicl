e040e711636a9c2aeb1194b035f7815120b1a7fe
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-04-05 20:51:02 +0700

    Update: string-encode-and-decode - submission-21

--- Data Structures & Algorithms/string-encode-and-decode/submission-21.cs
+++ Data Structures & Algorithms/string-encode-and-decode/submission-21.cs
@@ -1,29 +1,29 @@
 public class Solution {
-    public string Encode(IList<string> strs)
-    {
-        var res = new List<string>();
 
-        foreach ( var str in strs )
-        {
-            res.Add(str.Length + "#" + str);
+    public string Encode(IList<string> strs) {
+        var res = string.Empty;
+        foreach (var str in strs) {
+            res += str.Length + "#" + str;
         }
-        return string.Join("", res);
+        return res;
     }
+
     public List<string> Decode(string s)
     {
         var res = new List<string>();
-        for (int i = 0; i < s.Length; i++)
+        var i = 0;
+        while (i < s.Length)
         {
-            var j = i + 1;
+            var j = i;
             while (s[j] != '#')
             {
                 j++;
             }
-            var check = int.TryParse(s[i..j], out var number);
-            var end = j + number;
-            var current = s[(j + 1)..(end + 1)];
-            res.Add(current);
-            i = j + number;
+            var n = int.Parse(s[i..j]);
+            i = j + 1;
+            j = i + n;
+            res.Add(s[i..j]);
+            i = j;
         }
         return res;
     }
