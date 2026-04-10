eee87166827b43ac5d3181c7406fe5fb342cd987
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-04-10 21:20:57 +0700

    Add: three-integer-sum - submission-52

---
+++ Data Structures & Algorithms/three-integer-sum/submission-52.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        var res = new List<List<int>>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            var l = i + 1;
+            var r = nums.Length - 1;
+            while (l < r) {
+                var sum = nums[l] + nums[r] + nums[i];
+                if (sum < 0) {
+                    l++;
+                }
+                else if (sum > 0) {
+                    r--;
+                } 
+                else {
+                    res.Add([nums[l], nums[i], nums[r]]);
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
