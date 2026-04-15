c426cc68f9c5d989fc88d48e76ccdb89bd1af2c3
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-04-15 20:18:01 +0700

    Add: two-integer-sum-ii - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-15.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        var l = 0;
+        var r = numbers.Length - 1;
+
+        while (l < r) {
+            var current = numbers[l] + numbers[r];
+            if (current < target) {
+                l++;
+            }
+            else if (current > target) {
+                r --;
+            }
+            else {
+                return [l + 1, r + 1];
+            }
+        }
+        return [];
+    }
+}
