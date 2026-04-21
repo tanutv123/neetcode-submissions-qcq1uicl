57c4e7074ff50961aa364534b69cab31c2f36c2b
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-04-21 21:51:29 +0700

    Add: sliding-window-maximum - submission-35

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-35.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        var q = new LinkedList<int>();
+        var res = new int[nums.Length - k + 1];
+        var l = 0;
+        var r = 0;
+
+        while (r < nums.Length) {
+            while (q.Count > 0 && nums[q.Last.Value] < nums[r]) {
+                q.RemoveLast();
+            }
+            q.AddLast(r);
+            if (l > q.First.Value) {
+                q.RemoveFirst();
+            }
+            if (r + 1 >= k) {
+                res[l] = nums[q.First.Value];
+                l++;
+            }
+            r++;
+        }
+        return res;
+    }
+}
