7baefd2b14e1dcaf970e4bb6181f937a239390bc
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-04-22 18:57:46 +0700

    Add: validate-parentheses - submission-33

---
+++ Data Structures & Algorithms/validate-parentheses/submission-33.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var dict = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' },
+        };
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dict.ContainsKey(s[i]))
+            {
+                stack.Push(s[i]);
+                continue;
+            }
+            if (dict.ContainsKey(s[i]) )
+            {
+                if (stack.Count <= 0 || stack.Peek() != dict[s[i]]) {
+                    return false;
+                }
+                stack.Pop();
+            }
+        }
+        return stack.Count == 0;
+    }
+}
+
+/*
+([{}))
+
+
+*/
