507ca6a09f1682b3b3c9bb35ec3c1bc3c198e3ce
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-05-11 15:41:22 +0700

    Add: car-fleet - submission-44

---
+++ Data Structures & Algorithms/car-fleet/submission-44.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+        var pairs = new int[position.Length][];
+        for (int i = 0; i < position.Length; i++) {
+            pairs[i] = [position[i], speed[i]];
+        }
+        Array.Sort(pairs, (a, b) => b[0].CompareTo(a[0]));
+        var stack = new Stack<double>();
+        for (int i = 0; i < position.Length; i++) {
+            var t = (double)(target - pairs[i][0]) / pairs[i][1];
+            if (stack.Count == 0 || stack.Peek() < t) {
+                stack.Push(t);
+            } 
+        }
+        return stack.Count;
+    }
+}
