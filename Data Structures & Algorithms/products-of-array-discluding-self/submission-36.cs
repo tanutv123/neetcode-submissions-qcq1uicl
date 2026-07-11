2f8c85d1280fccbce7d5c475f3d1115cb56cd143
Tấn Tài <134121340+tanutv123@users.noreply.github.com>
2026-07-11 19:44:37 +0700

    Add: products-of-array-discluding-self - submission-36

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-36.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var prefix = new int[nums.Length];
+        prefix[0] = 1;
+        var suffix = new int[nums.Length];
+        suffix[nums.Length - 1] = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            prefix[i] = nums[i - 1] * prefix[i - 1];
+        }
+
+        for (int i = nums.Length - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            nums[i] = prefix[i] * suffix[i];
+        }
+        return nums;
+    }
+}
